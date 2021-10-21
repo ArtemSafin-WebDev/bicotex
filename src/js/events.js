33ba00b56493e70ce9bc3e521090cd0d8b609f42
Artem Safin <artemsafin.webdev@gmail.com>
2021-10-21 23:28:08 +0300

    first commit

---
+++ src/js/events.js
@@ -0,0 +1,22 @@
+import { Swiper, Navigation } from 'swiper';
+
+Swiper.use([Navigation]);
+
+export default function events() {
+    const elements = Array.from(document.querySelectorAll('.js-events'));
+
+    elements.forEach(element => {
+        const container = element.querySelector('.swiper-container');
+
+        new Swiper(container, {
+            slidesPerView: 'auto',
+            watchOverflow: true,
+            spaceBetween: 20,
+            speed: 400,
+            navigation: {
+                nextEl: element.querySelector('.slider-arrows__btn--next'),
+                prevEl: element.querySelector('.slider-arrows__btn--prev')
+            }
+        });
+    });
+}
