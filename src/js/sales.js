56a64a3b91d7f2a6665e891dfef275dba962eaa8
Artem Safin <artemsafin.webdev@gmail.com>
2021-11-28 18:43:19 +0300

    Правки

--- src/js/sales.js
+++ src/js/sales.js
@@ -8,26 +8,34 @@ export default function sales() {
         '(min-width: 641px)': () => {
             const elements = Array.from(document.querySelectorAll('.js-sales'));
 
+            const SHIFT_AMOUNT = 80;
+
             elements.forEach(element => {
-                const cards = Array.from(element.querySelectorAll('.sales__card'));
+                const cards = Array.from(element.querySelectorAll('.sales__list-item'));
 
                 cards.forEach((card, cardIndex) => {
                     const tl = gsap.timeline({
                         scrollTrigger: {
                             trigger: card,
                             start: 'top bottom',
-                            end: 'bottom top',
+                            end: '+=100%',
                             scrub: true
                         }
                     });
-                    if (cardIndex % 2 === 0) {
+
+                    if (cardIndex === 0) {
                         tl.to(card, {
-                            y: -40,
+                            y: SHIFT_AMOUNT / 2,
                             duration: 0.4
                         });
-                    } else {
+                    } else if (cardIndex === 1) {
+                        tl.from(card, {
+                            y: SHIFT_AMOUNT,
+                            duration: 0.4
+                        });
+                    } else if (cardIndex === 2) {
                         tl.to(card, {
-                            y: 40,
+                            y: SHIFT_AMOUNT,
                             duration: 0.4
                         });
                     }
