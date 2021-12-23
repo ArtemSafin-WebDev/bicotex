d31fd92b3897aa62d9a39d216b12811d339cea69
Artem Safin <artemsafin.webdev@gmail.com>
2021-12-23 04:24:58 +0300

    Модалка каталога

--- src/assets/js/backend.js
+++ src/assets/js/backend.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const catalog = document.querySelector('.catalog');
+
+    if (catalog) {
+        setTimeout(() => {
+            window.openModal('#catalog-modal');
+        }, 1000);
+    }
 });
