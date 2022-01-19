a4c815812ee18f46e0333720b55435635c0c18b4
Artem Safin <artemsafin.webdev@gmail.com>
2022-01-19 19:27:20 +0300

    По задачам

--- build/assets/js/backend.js
+++ build/assets/js/backend.js
@@ -6,4 +6,55 @@ document.addEventListener('DOMContentLoaded', () => {
             window.openModal('#catalog-modal');
         }, 1000);
     }
+
+    let productForm = document.querySelector('#product-form');
+
+    if (productForm) {
+        productForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            if (
+                $(productForm)
+                    .parsley()
+                    .isValid()
+            ) {
+                productForm.reset();
+                $(productForm)
+                    .parsley()
+                    .reset();
+                const success = document.querySelector('.product__form-success');
+                if (success) {
+                    success.classList.add('shown');
+
+                    setTimeout(() => {
+                        success.classList.remove('shown');
+                    }, 6000);
+                }
+            }
+        });
+    }
+    let careerForm = document.querySelector('#career-form');
+
+    if (careerForm) {
+        careerForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            if (
+                $(careerForm)
+                    .parsley()
+                    .isValid()
+            ) {
+                careerForm.reset();
+                $(careerForm)
+                    .parsley()
+                    .reset();
+                const success = document.querySelector('.career__form-success');
+                if (success) {
+                    success.classList.add('shown');
+
+                    setTimeout(() => {
+                        success.classList.remove('shown');
+                    }, 6000);
+                }
+            }
+        });
+    }
 });
